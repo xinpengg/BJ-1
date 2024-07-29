80f34d2d2f82df228e3474dd5ba14e5eadb2aaee
hungryTechie <iirimina@schools.nyc.gov>
2024-07-29 15:33:01 -0400

    First Commit of the starter BJ files

---
+++ index.js
@@ -0,0 +1,19 @@
+// Build a BlackJack Game
+
+// variables representing two cards 
+let firstCard = 10
+let secondCard = 4
+
+//challenge #1
+//create a new variable and set it to the sum of the two cards 
+
+
+//chalenge #2 
+// code the statements below based on the image called blackjack.png in the images folder
+// your output should work in the browser's console based on the game logic and conditional statements 
+
+//starter code
+    message = "Do you want to draw a new card? 🙂"
+    message = "Wohoo! You've got Blackjack! 🥳"
+    message = "You're out of the game! 😭"
+console.log(message)
