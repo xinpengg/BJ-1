cb407670789918719f576288a5e937296d61cda6
hungryTechie <iirimina@schools.nyc.gov>
2024-07-30 11:54:45 -0400

    updating index.js

--- index.js
+++ index.js
@@ -9,8 +9,8 @@ let secondCard = 4
 
 
 //chalenge #2 
-// code the statements below based on the image called blackjack.png in the images folder
-// your output should work in the browser's console based on the game logic and conditional statements 
+// code the statements below based on the conditions shown in blackjack.png from the images folder
+// your output should work in the browser's console based on changing the values assigned to the cards
 
 //starter code
     message = "Do you want to draw a new card? 🙂"
